c76e4d373ff5e34620432a2daef7adddf5ed6aa9
Crowdbotics <engineering@crowdbotics.com>
2023-08-26 23:14:31 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "frosty-bonus-dev-111792",
+  url: "https://frosty-bonus-dev-111792.botics.co",
+  api: "https://frosty-bonus-dev-111792.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
